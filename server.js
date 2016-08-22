766c3786bf51d1415679ef1879688ef29e7dc022
WESTBROOK\ptirumalaset <tpraveen7@gmail.com>
2016-08-22 14:53:48 -0400

    no message

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+
+app.get('/', function(request, response){
+  response.send( 'oh hai');
+
+});
+
 app.listen(3000,function(){
 console.log('Example app Listening on Port 3000:');
 
