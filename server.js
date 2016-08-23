4f26485fff19749e58380935817bf4d454493a52
WESTBROOK\ptirumalaset <tpraveen7@gmail.com>
2016-08-23 09:26:01 -0400

    no message

--- server.js
+++ server.js
@@ -3,23 +3,11 @@ var app = express();
 var fakearray = require('./model/fakearray.js');
 var PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  console.log('middleware doing stuff');
-  next();
-})
-
-app.use(function(req, res, next){
-  console.log('second middleware doing stuff');
-  next();
-})
+var runController = requier('./controllers/runs.js');
+app.use('/runs', runController);
 
 
-app.use(function(req, res, next){
-  console.log('Third middleware doing stuff');
-  next();
-})
 
-console.log(fakearray);
 app.get('/', function(request, response){
   response.render('index.ejs', {
     dataArray:fakearray
@@ -27,10 +15,6 @@ app.get('/', function(request, response){
 });
 
 
-
-app.get('/foo',function(req, res){
-  res.send('works');
-})
 
 app.listen(PORT, function(){
 console.log('Example app Listening on Port 3000:');
